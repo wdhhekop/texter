db8619ea3c5360746aef7bee14dcd770aca36db4
wdhhekop <ninilina0@mail.ru>
2024-03-17 23:43:03 +0500

    Окно редактора с меню (кнопки: "открыть", "сохранить", "выход"), текстовым полем.

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QAction, QFileDialog, QPlainTextEdit
 
-import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Текстовый редактор")
 
-class Window(QMainWindow):
-    def __int__(self):
-        super(Window, self).__init__()
-
-        self.setWindowTitle("Редактор текста")
-        self.setGeometry(300, 250, 350, 200)
-
-        self.text_edit = QtWidgets.QTextEdit(self)
+        # Создаем текстовое поле
+        self.text_edit = QPlainTextEdit()
         self.setCentralWidget(self.text_edit)
 
+        # Создаем меню
+        self.menu_bar = QMenu("Меню")
+        self.menu_bar.addAction("Открыть", self.open_file)
+        self.menu_bar.addAction("Сохранить", self.save_file)
+        self.menu_bar.addSeparator()
+        self.menu_bar.addAction("Выход", self.close)
 
-def application():
-    
+        # Добавляем меню в главное окно
+        self.menuBar().addMenu(self.menu_bar)
+
+    def open_file(self):
+        # Открываем диалоговое окно для выбора файла
+        filename, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "", "Текстовые файлы (*.txt)")
+
+        if filename:
+            # Открываем файл и читаем его содержимое
+            with open(filename, "r") as f:
+                text = f.read()
+
+            # Задаем текст в текстовом поле
+            self.text_edit.setPlainText(text)
+
+    def save_file(self):
+        # Открываем диалоговое окно для сохранения файла
+        filename, _ = QFileDialog.getSaveFileName(self, "Сохранить файл", "", "Текстовые файлы (*.txt)")
+
+        if filename:
+            # Получаем текст из текстового поля
+            text = self.text_edit.toPlainText()
+
+            # Сохраняем текст в файл
+            with open(filename, "w") as f:
+                f.write(text)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec_()
+
+
